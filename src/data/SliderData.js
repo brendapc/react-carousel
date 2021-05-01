8f92e867538b9cb04818c4501b5171d1b07841d6
Brenda Pereira <brendacamara@outlook.com>
2021-05-01 16:00:04 -0300

    feat: add images data

---
+++ src/data/SliderData.js
@@ -0,0 +1,20 @@
+export const SliderData = [
+    {
+        image: "https://images.unsplash.com/photo-1618811213393-5f399dab320e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1048&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1576207605201-48495f8ae122?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1054&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1618679343069-2c148023c592?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1618593652021-3dc439bf24c1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1614771195442-37e16513ef57?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1027&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1617575521841-5ef02d932f9c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+    },
+]
